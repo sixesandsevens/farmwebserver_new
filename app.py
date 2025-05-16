bafab220346c4c2f6396721a215c9ecfdaba10ef
sixesandsevens <darkseren@gmail.com>
2025-05-16 11:39:12 -0400

    dupe

--- app.py
+++ app.py
@@ -60,18 +60,6 @@ app.config.update({
     )
 })
 
-# Initialize extensions
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///farm.db'
-app.config['SECRET_KEY'] = 'your-secret-key'
-
-db.init_app(app)
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
-
 
 #robots.txt
 
