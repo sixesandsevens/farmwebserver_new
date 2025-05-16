423f6b3ce9fb7feb22e139de978811723366565c
sixesandsevens <darkseren@gmail.com>
2025-05-16 13:44:30 -0400

    truncated hash

--- models.py
+++ models.py
@@ -9,7 +9,7 @@ class User(UserMixin, db.Model):
     id            = db.Column(db.Integer, primary_key=True)
     username      = db.Column(db.String(80), unique=True, nullable=False)
     email         = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(256), nullable=False)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
