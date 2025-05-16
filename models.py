74ac64adbb299d2edacf008e97b1c49f1da22f37
sixesandsevens <darkseren@gmail.com>
2025-05-16 13:54:05 -0400

    hash

--- models.py
+++ models.py
@@ -2,6 +2,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_login       import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
+from sqlalchemy import Text
 
 db = SQLAlchemy()
 
@@ -9,7 +10,7 @@ class User(UserMixin, db.Model):
     id            = db.Column(db.Integer, primary_key=True)
     username      = db.Column(db.String(80), unique=True, nullable=False)
     email         = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(256), nullable=False)
+    password_hash = db.Column(Text, nullable=False)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
